ad78a3e2af697e8e918968961dbc573a4e64b718
Connor Janowiak <connor.janowiak@anotheryeti.com>
2024-03-15 22:51:04 -0600

    Basic cardflipping interface working

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+allcards = document.querySelectorAll(".card");
+
+i = 0;
+document.querySelectorAll(".card").forEach((card) => {
+  card.style.zIndex = -i;
+  i++;
+});
 
 // Wrap every letter in a span
 // var textWrapper = document.querySelector('.wedding');
@@ -16,4 +23,25 @@ for(letter in letters) {
       loop: true,
       easing: "easeInOutSine",
     });
+}
+
+const clamp = (n, min, max) =>
+Math.min(Math.max(n, min), max)
+
+onscroll = (event) => {
+    percent = window.scrollY / (document.body.scrollHeight - window.innerHeight);
+    i = 0;
+    allcards.forEach((card) => {
+        start = i / allcards.length;
+        end = start + 1 / (allcards.length + 1)
+        rot = clamp(179 * (percent - start) / (end - start), 0, 179)
+        card.style.transform = `perspective(20cm) rotateX(${rot}deg)` 
+
+        if(rot > 90) {
+           card.style.zIndex = i;
+        } else {
+           card.style.zIndex = -i;
+        }
+        i++;
+    })
 }
