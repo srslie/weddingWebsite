ff66e54bcdae00705d696648ae01ea5042f889fb
srslie <aliceruppert@gmail.com>
2024-03-19 02:16:46 -0600

    Address issues both substantial and irrelevant as well as introduce certain behaviors that may or or may not provide the intended user experiences to those who dare to dream of a future containing content, and form, and function, as well as a certain regard for the love of my life

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 const allcards = document.querySelectorAll(".card");
-const numberOfCards = Math.min(allcards.length/2);
-const positionYperCard = (document.body.scrollHeight - window.innerHeight) / numberOfCards;
 
 //event listeners
 window.addEventListener('scroll', handleScroll);
@@ -27,95 +25,34 @@ function handleNavToTimeline(event) {
 }
 function handleNavToTravel(event) {
     console.log('inNavToTravel');
-    const topPosition = positionYperCard * 2;
-    window.scrollTo({
-        top: topPosition,
-        left: 0,
-        behavior: "smooth",
-    });
+    scrollToPosition(2)
 }
 
-function boozier(p0, p1, p2, p3, t) {
-    let oneMinus = 1 - t;
-    return (
-        oneMinus * oneMinus * oneMinus * p0 +
-        3 * oneMinus * oneMinus * t * p1 +
-        3 * oneMinus * t * t * p2 +
-        t * t * t * p3
-    );
-
-    // cubic-bezier(0.645, 0.045, 0.355, 1);
+function scrollToPosition(card) {
+    console.log('inscroll');
+    getElementById(card).scrollToView({ behavior: "smooth"});
 }
 
-i = 0;
-document.querySelectorAll(".card").forEach((card) => {
-    card.style.zIndex = -i;
+// const letters = document.querySelectorAll(".weddingletter");
+// for (letter in letters) {
+//     const startrot = Math.random() * 30 - 10;
+//     const endrot = Math.random() * 30 - 10;
+//     const min = 800;
+//     const max = 1100;
 
-    if(i%2 == 0) {
-        card.classList.add("bottom");
-    } else {
-        card.classList.add("top");
-    }
-
-    i++;
-});
-
-// Wrap every letter in a span
-// var textWrapper = document.querySelector('.wedding');
-// textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
-const letters = document.querySelectorAll(".weddingletter");
-for (letter in letters) {
-    startrot = Math.random() * 30 - 10;
-    endrot = Math.random() * 30 - 10;
-    anime({
-        targets: letters[letter],
-        keyframes: [{
-                translateY: [2, -2],
-                rotate: [startrot, endrot],
-                duration: anime.random(800, 1100),
-            },
-            {
-                translateY: [-2, 2],
-                rotate: [endrot, startrot],
-                duration: anime.random(800, 1100),
-            },
-        ],
-        loop: true,
-        easing: "easeInOutSine",
-    });
-}
-
-const clamp = (n, min, max) => Math.min(Math.max(n, min), max);
-
-function handleScroll(event) {
-    percent = window.scrollY / (document.body.scrollHeight - window.innerHeight);
-    let i = 0;
-    allcards.forEach((card) => {
-        let usingI = Math.floor(i/2);
-        const start = usingI / numberOfCards;
-        const end = start + 1 / (numberOfCards + 1);
-        let t = (percent - start) / (end - start);
-        if (t >= 0 && t <= 1) {
-            t = boozier(0, 1.1, 0.9, 1, t);
-            
-            // t = boozier(0.645, 0.045, 0.355, 1, t);
-        }
-
-        let rot = clamp(179 * t, 0, 179);
-        if(card.classList.contains("top")) {
-            rot = rot + 180;
-        }
-
-        card.style.transform = `perspective(20cm) rotateX(${rot}deg)`;
-
-        if (rot > 90) {
-            card.style.zIndex = usingI;
-        } else {
-            card.style.zIndex = -usingI;
-        }
-        i++;
-    });
-};
-
-window.onload = (event) => { handleScroll(null); }
+//     // loop: true;
+//     // easing: "easeInOutSine";
+//     letter.animate({
+//         keyframes: [{
+//                 translateY: [2, -2],
+//                 rotate: [startrot, endrot],
+//                 duration: Math.floor(Math.random() * (max - min  + 1)),
+//             },
+//             {
+//                 translateY: [-2, 2],
+//                 rotate: [endrot, startrot],
+//                 duration: Math.floor(Math.random() * (max - min  + 1))
+//             },
+//         ],
+//     });
+// }
